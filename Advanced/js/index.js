ccbc7a8624dbbcf65853366471e3ee67f35bc872
Barbara Anissin <anissinb@gmail.com>
2019-10-09 13:17:50 +0200

    advanced works

--- Advanced/js/index.js
+++ Advanced/js/index.js
@@ -1,13 +1,13 @@
 const BOOK = {
     books: [
-      {"title": "Javascript for Dummies","author": "Dummy Dumb Dumb", "read": "false", "cover": "./img/Download.jpg"},
-      {"title": "Javascript for Kids","author": "Nick Morgan", "read": "false", "cover": "https://wsvincent.com/assets/images/bestjavascriptbooks/js_for_kids.jpg"},
-      {"title": "Up and Going Javascript","author": "Kyle Simpson", "read": "true", "cover": "https://images-eu.ssl-images-amazon.com/images/I/41L18FvA5rL.jpg"},
+        { "title": "Javascript for Dummies", "author": "Dummy Dumb Dumb", "read": "false", "cover": "./img/Download.jpg" },
+        { "title": "Javascript for Kids", "author": "Nick Morgan", "read": "false", "cover": "https://wsvincent.com/assets/images/bestjavascriptbooks/js_for_kids.jpg" },
+        { "title": "Up and Going Javascript", "author": "Kyle Simpson", "read": "true", "cover": "https://images-eu.ssl-images-amazon.com/images/I/41L18FvA5rL.jpg" },
     ]
 }
 
-function generateTable () {
-	
+function generateTable() {
+
     let tableHtml = [];
     for (let i = 0; i < BOOK.books.length; i++) {
         let htmlString = `
@@ -16,19 +16,32 @@ function generateTable () {
                 	<p><img src=${BOOK.books[i].cover}></p>
                 	<p><b>Title: ${BOOK.books[i].title}</b></p>
                 	<p>Author: ${BOOK.books[i].author}</p>
-                	<p class="read">${BOOK.books[i].read}</p>
+                	<p id="read${i}">${BOOK.books[i].read}</p>
                 </div>
             </li>
         `
         tableHtml.push(htmlString);
-        
+
     }
-    
+
     tableHtml.unshift(`<ul class='book-list'>`); //Places opening ul tag at beginning of the array
-    tableHtml.push(`</ul>`);//Places closing ul tag at the end of the array
-    return (tableHtml.join(''));//Turns seperate html elements into one html element.
+    tableHtml.push(`</ul>`); //Places closing ul tag at the end of the array
+    return (tableHtml.join('')); //Turns seperate html elements into one html element.
 
 
 }
 
 $('body').html(generateTable);
+
+function changeCol() {
+    
+    for (var i = 0; i < 4; i++) {
+    	var lesen = BOOK.books[i].read
+        if (lesen == "true") {
+        	document.getElementById("read"+i).style.backgroundColor = "yellowgreen";
+        } else {
+            document.getElementById("read"+i).style.backgroundColor = "hotpink";
+        }
+    }
+}
+changeCol();
